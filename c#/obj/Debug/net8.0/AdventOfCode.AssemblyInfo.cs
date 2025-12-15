6d687f344a62815973c65295cfa9111408a79b90
Rafmon <rafmon.vrml@gmx.net>
2025-12-15 10:47:06 +0100

    added go

--- c#/obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
+++ c#/obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f86e5a4ef086844dd509a17b95f52e68d95bc31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b9c268d735d9497b146c051c516225535f9e3dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- java/src/de/rafmon/aoc/_24/_5/PrintQueue.java
+++ java/src/de/rafmon/aoc/_24/_5/PrintQueue.java
@@ -5,60 +5,88 @@ import java.util.HashSet;
 import de.rafmon.aoc.util.InputHelper;
 
 public class PrintQueue {
-	private static int fixedPagesAccumulatedPageNumbers;
-	
+	private static final String PAGE_SEPERATOR = "|";
+	private int fixedPagesAccumulatedPageNumbers = 0;
+	private int accumulatedPageNumbers = 0;
+
 	public static void main(String[] args) {
+		PrintQueue printQueue = new PrintQueue();
+		printQueue.runPrintQueue();
+	}
+
+	private void runPrintQueue() {
 		String[] inputSplit = PrintQueueInput.INPUT.split("\r\n\r\n");
-		
-		InputHelper.convertToStringArray(inputSplit[0]);
-		HashSet<String> rulebook = new HashSet<>(1200);
-	
-		for(String rule : InputHelper.convertToStringArray(inputSplit[0])) {
-			rulebook.add(rule);
-		}
-		int accumulatedPageNumbers = 0;
-		
-		for (String string : InputHelper.convertToStringArray(inputSplit[1])) {
-			accumulatedPageNumbers += check(string.split(","), rulebook);
-		}
-		
+
+		HashSet<String> rulebook = createRuleBook(inputSplit);
+
+		calculateAccumulatedPagesSum(InputHelper.convertToStringArray(inputSplit[1]), rulebook);
+
 		System.out.println("Accumulated middle page numbers: " + accumulatedPageNumbers);
 		System.out.println("Fixed pages accumulated page numbers: " + fixedPagesAccumulatedPageNumbers);
 	}
 
-	
-	private static int check(String[] pages, HashSet<String> rulebook) {
-		for(int i = 0; i < pages.length; i++) {
-			for(int j = i+1;  j < pages.length; j++) {
-				if(!accordingToRules(pages[i],pages[j], rulebook)) {
-					fixedPagesAccumulatedPageNumbers += fixPages(pages,rulebook);
-				}
-			}
+	private void calculateAccumulatedPagesSum(String[] allPageGroups, HashSet<String> rulebook) {
+		for (String pageGroup : allPageGroups) {
+			accumulatedPageNumbers += validateAndCheckPages(pageGroup.split(","), rulebook);
 		}
-		return Integer.valueOf(pages[(pages.length/2)]);
 	}
 
-	//solution for pt2
-	private static int fixPages(String[] pages, HashSet<String> rulebook) {
-		while(true) {
-			boolean foundFault = false;
-			for(int i = 0; i < pages.length-1; i++) {
-				if(!accordingToRules(pages[i],pages[i+1], rulebook)) {
-					String temp = pages[i];
-					pages[i] = pages[i+1];
-					pages[i+1] = temp;
-					foundFault = true;
-				}
-			}
-			if(!foundFault) {
-				return Integer.valueOf(pages[(pages.length/2)]);
+	private HashSet<String> createRuleBook(String[] inputSplit) {
+		int estimatedRulebookSize = 1200;
+		HashSet<String> rulebook = new HashSet<>(estimatedRulebookSize);
+
+		for (String rule : InputHelper.convertToStringArray(inputSplit[0])) {
+			rulebook.add(rule);
+		}
+		return rulebook;
+	}
+
+	private int validateAndCheckPages(String[] pages, HashSet<String> rulebook) {
+		if (!arePagesValid(pages, rulebook)) {
+			fixedPagesAccumulatedPageNumbers += fixInvalidPages(pages, rulebook);
+		}
+		return extractMiddlePageNumber(pages);
+	}
+
+	private boolean arePagesValid(String[] pages, HashSet<String> rulebook) {
+		for (int i = 0; i < pages.length - 1; i++) {
+			if (!accordingToRules(pages[i], pages[i+1], rulebook)) {
+				return false;
 			}
 		}
-		
+		return true;
+
+	}
+
+	private Integer extractMiddlePageNumber(String[] pages) {
+		return Integer.valueOf(pages[(pages.length / 2)]);
+	}
+
+	// solution for pt2
+	private int fixInvalidPages(String[] pages, HashSet<String> rulebook) {
+		while (true) {
+			boolean isInvalid = false;
+			for (int pageIndex = 0; pageIndex < pages.length - 1; pageIndex++) {
+				if (!accordingToRules(pages[pageIndex], pages[pageIndex + 1], rulebook)) {
+					swapPages(pages, pageIndex);
+					isInvalid = true;
+				}
+			}
+			if (!isInvalid) {
+				return extractMiddlePageNumber(pages);
+			}
+		}
+
+	}
+
+	private void swapPages(String[] pages, int i) {
+		String temp = pages[i];
+		pages[i] = pages[i + 1];
+		pages[i + 1] = temp;
 	}
 
 	private static boolean accordingToRules(String x, String y, HashSet<String> rulebook) {
-		return (rulebook.contains(x + "|" + y));
-		
+		return (rulebook.contains(x + PAGE_SEPERATOR + y));
+
 	}
 }
